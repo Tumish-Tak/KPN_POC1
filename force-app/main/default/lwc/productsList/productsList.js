d610c87a78026bcd0f703a2510246f5eee79d078
Tumish-Tak <tumishtk@gmail.com>
2021-09-21 19:52:53 +0200

    sorting added to ProductList LWC

--- force-app/main/default/lwc/productsList/productsList.js
+++ force-app/main/default/lwc/productsList/productsList.js
@@ -1,13 +1,17 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getProductList from '@salesforce/apex/ProductListController.getProductList';
 const COLUMNS = [
-    {label:  'Name' , fieldName:  'ProductName' , type:  'text'},
-    {label:  'List Price' , fieldName:  'UnitPrice' , type:  'currency'}       
-]; 
+    {label:  'Name' , fieldName:  'ProductName' , type:  'text', sortable: true},
+    {label:  'List Price' , fieldName:  'UnitPrice' , type:  'currency', sortable: true}       
+];
+
 export default class ProductsList extends LightningElement {
     @api recordId;
     @track productList;
     @track columns = COLUMNS;
+    defaultSortDirection = 'asc';
+    sortDirection = 'asc';
+    sortedBy;
 
     @wire (getProductList, {orderid:  '$recordId' }) 
     WireProductRecords({error, data}){
@@ -26,5 +30,31 @@ export default class ProductsList extends LightningElement {
             this.error = error;
             this.productList =  undefined ;
         }
+    }
+
+    sortBy(field, reverse, primer) {
+        const key = primer
+            ? function (x) {
+                  return primer(x[field]);
+              }
+            : function (x) {
+                  return x[field];
+              };
+
+        return function (a, b) {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        };
+    }
+
+    onHandleSort(event) {
+        const { fieldName: sortedBy, sortDirection } = event.detail;
+        const cloneData = [...this.productList];
+
+        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+        this.productList = cloneData;
+        this.sortDirection = sortDirection;
+        this.sortedBy = sortedBy;
     }
 }
