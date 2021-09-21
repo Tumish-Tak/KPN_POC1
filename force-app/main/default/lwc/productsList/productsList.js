82fcc1212e08cfc5c77136e85376742ffd6571f4
Tumish-Tak <tumishtk@gmail.com>
2021-09-21 19:19:54 +0200

    corrected js error

--- force-app/main/default/lwc/productsList/productsList.js
+++ force-app/main/default/lwc/productsList/productsList.js
@@ -1,19 +1,28 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getProductList from '@salesforce/apex/ProductListController.getProductList';
+const COLUMNS = [
+    {label:  'Name' , fieldName:  'ProductName' , type:  'text'},
+    {label:  'List Price' , fieldName:  'UnitPrice' , type:  'currency'}       
+]; 
 export default class ProductsList extends LightningElement {
     @api recordId;
     @track productList;
-    @track columns = [
-        {label:  'Name' , fieldName:  'Product2.Name' , type:  'text'},
-        {label:  'List Price' , fieldName:  'UnitPrice' , type:  'currency'}
-    ];
+    @track columns = COLUMNS;
 
     @wire (getProductList, {orderid:  '$recordId' }) 
-    WireProductRecords ({error, data}) {
-        if (data) {
-            this.productList = data;
+    WireProductRecords({error, data}){
+        if(data){
+            let preparedProducts = [];
+            data.forEach(prodtRec =>{
+                let preparedProduct = {};
+                preparedProduct.id = prodtRec.Product2ID;
+                preparedProduct.ProductName = prodtRec.Product2.Name;
+                preparedProduct.UnitPrice = prodtRec.UnitPrice;
+                preparedProducts.push(preparedProduct);
+            });
+            this.productList = preparedProducts;
             this.error =  undefined ;
-        } else {
+        } else{
             this.error = error;
             this.productList =  undefined ;
         }
